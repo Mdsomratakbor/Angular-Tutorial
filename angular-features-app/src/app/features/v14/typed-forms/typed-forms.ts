750e2780bbb6c59357e87c68334de38958df5782
Md Somrat Akbor <somrat876@gmail.com>
2025-07-30 10:15:10 +0600

    add v17 routes

--- angular-features-app/src/app/features/v14/typed-forms/typed-forms.ts
+++ angular-features-app/src/app/features/v14/typed-forms/typed-forms.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 export interface ContactForm {
   name: FormControl<string>;
   email: FormControl<string>;
   message: FormControl<string|null>;
+}
+
+export interface PhoneForm{
+  phones: FormArray<FormControl<number|null>>
 }
 @Component({
   selector: 'app-typed-forms',
@@ -14,19 +18,43 @@ export interface ContactForm {
 
 export class TypedForms {
  contactForm: FormGroup<ContactForm>;
+ phoneForm: FormGroup<PhoneForm>;
 
   constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: new FormControl('', {nonNullable:true}),
-      email: new FormControl('', {nonNullable:true}),
+      email: new FormControl('',   {validators: [Validators.required, Validators.email], nonNullable:true}),
       message: new FormControl('', {nonNullable:false})
     });
+
+    this.phoneForm = this.fb.group({
+      phones: this.fb.array([this.fb.control<number|null>(null, Validators.required)])
+    })
   }
 
-  onSubmit() {
+  onSubmitContact() {
     if (this.contactForm.valid) {
       const formValue = this.contactForm.value;
       console.log('Typed Form Value:', formValue);
+    }
+  }
+
+  
+  get phones(): FormArray<FormControl<number | null>> {
+    return this.phoneForm.get('phones') as FormArray<FormControl<number | null>>;
+  }
+
+  addPhone() {
+    this.phones.push(this.fb.control<number | null>(null, Validators.required));
+  }
+
+  removePhone(index: number) {
+    this.phones.removeAt(index);
+  }
+
+  onSubmitPhones() {
+    if (this.phoneForm.valid) {
+      console.log(this.phoneForm.value);
     }
   }
 }
--- angular-features-app/src/app/features/v17/v17-routing-module.ts
+++ angular-features-app/src/app/features/v17/v17-routing-module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { V17 } from './v17';
+import { ControlFlow } from './control-flow/control-flow';
+import { StandalonePreview } from '../v14/standalone-preview/standalone-preview';
+import { ExtendedDiagnostics } from '../v14/extended-diagnostics/extended-diagnostics';
+import { DeferredLoading } from './deferred-loading/deferred-loading';
+import { I18nPipes } from './i18n-pipes/i18n-pipes';
+import { RouterEnhancements } from './router-enhancements/router-enhancements';
+import { SignalForms } from './signal-forms/signal-forms';
 
-const routes: Routes = [{ path: '', component: V17 }];
+const routes: Routes = [
+  { path: '', component: V17 },
+    { path: 'control-flow', component: ControlFlow },
+  { path: 'standalone-preview', component: StandalonePreview },
+  { path: 'extended-diagnostics', component: ExtendedDiagnostics },
+  { path: 'deferred-loading', component: DeferredLoading },
+  { path: 'i18n-pipes', component: I18nPipes },
+  { path: 'router-enhancements', component: RouterEnhancements },
+  { path: 'signal-forms', component: SignalForms }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
